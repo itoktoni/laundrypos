b84eb12fd429403fedd96780e0d1301fc8678ab5
Itok toni - Ryzen <itok.toni@gmail.com>
2026-08-22 08:19:01 +0700

    feat: order-related enums

---
+++ app/Enums/MetodePembayaranEnum.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Enums;
+
+use App\Concerns\EnumTrait;
+use BenSampo\Enum\Enum;
+
+final class MetodePembayaranEnum extends Enum
+{
+    use EnumTrait;
+
+    const TUNAI = 'tunai';
+    const TRANSFER = 'transfer';
+    const DOMPET_DIGITAL = 'dompet_digital';
+
+    public static function getDescription(mixed $value): string
+    {
+        return match ($value) {
+            self::TUNAI => 'Tunai',
+            self::TRANSFER => 'Transfer',
+            self::DOMPET_DIGITAL => 'Dompet Digital',
+            default => parent::getDescription($value),
+        };
+    }
+}
---
+++ app/Enums/MetodePengambilanEnum.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Enums;
+
+use App\Concerns\EnumTrait;
+use BenSampo\Enum\Enum;
+
+final class MetodePengambilanEnum extends Enum
+{
+    use EnumTrait;
+
+    const ANTAR_TOKO = 'antar_toko';
+    const JEMPUT = 'jemput';
+
+    public static function getDescription(mixed $value): string
+    {
+        return match ($value) {
+            self::ANTAR_TOKO => 'Antar ke Toko',
+            self::JEMPUT => 'Dijemput',
+            default => parent::getDescription($value),
+        };
+    }
+}
